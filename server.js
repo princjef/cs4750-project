0138ea97020b9213bfaaaa8a2758a54f842c52e2
Jeff Principe <princjef@gmail.com>
2013-11-09 09:41:11 -0500

    Added route for creating tournament

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var app = express();
 var config = require('./config.json');
 var connection = mysql.createConnection(config);
 
+// Compression
 app.use(express.compress());
-app.use(express.static(__dirname + '/app'));
+
+// Static Server
+app.use('/js', express.static(__dirname + '/app/js'));
+app.use('/partials', express.static(__dirname + '/app/partials'));
+
+// All other routes
+app.all('/*', function(req, res) {
+	console.log("GET", req.path);
+	res.sendfile('./app/index.html');
+});
 
 app.listen(8080);
 console.log('Listening on port 8080');
