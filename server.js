e7c280b30554a6244f34a5cf8c6dc3988d0cd631
Jeff Principe <princjef@gmail.com>
2013-11-09 08:08:58 -0500

    Added mysql connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require('express');
+var express = require('express'),
+	mysql = require('mysql');
+
 var app = express();
+var config = require('./config.json');
+var connection = mysql.createConnection(config);
 
 app.use(express.compress());
 app.use(express.static(__dirname + '/app'));
