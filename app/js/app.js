2c763a66aae72b408edc39c69ef44b0006c954d3
Jeff Principe <princjef@gmail.com>
2013-11-09 22:36:40 -0500

    Added DB query for tournament types

--- app/js/app.js
+++ app/js/app.js
@@ -10,11 +10,31 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 	$locationProvider.html5Mode(true).hashPrefix('!');
 }]);
 
+app.service('$dropdowns', ['$q', '$http', function($q, $http) {
+	return {
+		getTournamentLevels: function() {
+			var d = $q.defer();
+			$http({
+				method: 'GET',
+				url: '/tournament/levels',
+				cache: true
+			}).success(function(data) {
+				d.resolve(data);
+			}).error(function(err) {
+				d.reject(err);
+			});
+			return d.promise;
+		}
+	};
+}]);
+
 app.controller('PageCtrl', ['$scope', function($scope) {
 	$scope.text = 'Hello World';
 	console.log('Hello World');
 }]);
 
-app.controller('TournamentCreateCtrl', ['$scope', function($scope) {
-
+app.controller('TournamentCreateCtrl', ['$scope', '$dropdowns', function($scope, $dropdowns) {
+	$dropdowns.getTournamentLevels().then(function(data) {
+		$scope.levels = data;
+	});
 }]);
--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.use(express.compress());
 app.use('/js', express.static(__dirname + '/app/js'));
 app.use('/partials', express.static(__dirname + '/app/partials'));
 
+app.get('/tournament/levels', function(req, res) {
+	connection.query("SHOW COLUMNS FROM Tournament LIKE 'tournamentType'", function(err, rows) {
+		if(err) {
+			console.log('ERR', err);
+			res.send(500);
+		} else {
+			var match = rows[0].Type.match(/^enum\(\'(.*)\'\)$/)[1];
+			var levels = match.split('\',\'');
+			res.json(levels);
+		}
+	});
+});
+
 // All other routes
 app.all('/*', function(req, res) {
 	console.log("GET", req.path);
