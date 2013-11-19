b5e34db22d691b849dcc9300205c5292ee41e6fc
Richard Knoll <nirro64@gmail.com>
2013-11-18 23:55:16 -0500

    Added a form object to the EventCreateCtrl and an http request to create an event

--- app/js/app.js
+++ app/js/app.js
@@ -19,12 +19,22 @@ angular.module('scoreApp', ['ui.bootstrap', 'ngCookies'])
 angular.module('scoreApp').controller('PageCtrl', ['$scope', function($scope) {
 }]);
 angular.module('scoreApp').controller('EventCreateCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.form = {};
+	
 	$scope.divisions = [
 	{value:'A'}, 
 	{value:'B'}, 
 	{value:'C'}
 	];
-	$scope.division = $scope.divisions[0];
+	
+	$scope.form.division = $scope.divisions[0];
+	
+	$scope.create = function() {
+		$http({
+			method:'POST',
+			url:'/event/create',
+			data:$scope.form});
+	};
 }]);
 
 
--- app/js/controllers/event/EventCreateCtrl.js
+++ app/js/controllers/event/EventCreateCtrl.js
@@ -1,9 +1,19 @@
 angular.module('scoreApp').controller('EventCreateCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.form = {};
+	
 	$scope.divisions = [
 	{value:'A'}, 
 	{value:'B'}, 
 	{value:'C'}
 	];
-	$scope.division = $scope.divisions[0];
+	
+	$scope.form.division = $scope.divisions[0];
+	
+	$scope.create = function() {
+		$http({
+			method:'POST',
+			url:'/event/create',
+			data:$scope.form});
+	};
 }]);
 
