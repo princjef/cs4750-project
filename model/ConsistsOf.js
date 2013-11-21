9342eb158e61ad75ad778c2e7d838304630b99e6
Richard Knoll <nirro64@gmail.com>
2013-11-21 16:47:08 -0500

    Fixed he parameters to the query method in the consistsOF model

--- model/ConsistsOf.js
+++ model/ConsistsOf.js
@@ -15,18 +15,35 @@ var ConsistsOf = function(obj) {
 	this.writerID = obj.writerID;
 };
 
-ConsistsOf.prototype.addEventToTournament = function() {
+ConsistsOf.prototype.addEventToTournament = function(callback) {
 	connection.query('INSERT INTO ConsistsOf(tournamentID, eventName, division, eventType,'+
 		' tiebreak, highScoreWins, highTiebreakWins, scored, supervisor_officialID, writer_officialID)'+
-		' VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', this.tournamentID, this.eventName, this.division, this.eventType,
-		this.tiebreak, this.highScoreWins, this.highTiebreakWins, this.scored, this.supervisorID, this.writerID);
+		' VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [this.tournamentID, this.eventName, this.division, this.eventType,
+		this.tiebreak, this.highScoreWins, this.highTiebreakWins, this.scored, this.supervisorID, this.writerID],
+		function(err, row) {
+			if(err) {
+				console.log(err);
+				callback(err);
+			} else {
+				console.log('Added an event to a tournament');
+				callback();
+			}
+		});
 };
 
-ConsistsOf.prototype.updateEventInTournament = function() {
+ConsistsOf.prototype.updateEventInTournament = function(callback) {
 	connection.query('UPDATE ConsistsOf SET tournamentID=?, eventName=?, division=?, eventType=?,'+
 		' tiebreak=?, highScoreWins=?, highTiebreakWins=?, scored=?, supervisor_officialID=?, writer_officialID=?)',
-		this.tournamentID, this.eventName, this.division, this.eventType, this.tiebreak, this.highScoreWins, 
-		this.highTiebreakWins, this.scored, this.supervisorID, this.writerID);
+		[this.tournamentID, this.eventName, this.division, this.eventType, this.tiebreak, this.highScoreWins, 
+		this.highTiebreakWins, this.scored, this.supervisorID, this.writerID], function(err, row) {
+			if(err) {
+				console.log(err);
+				callback(err);
+			} else {
+				console.log('Updated an event in a tournament');
+				callback();
+			}
+		});
 };
 
 
