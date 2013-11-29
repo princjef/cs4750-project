d691fe6fae66e03e75886300fa88f48ff872d354
Richard Knoll <nirro64@gmail.com>
2013-11-28 21:33:20 -0500

    Added Team update method to Team Model

--- model/Team.js
+++ model/Team.js
@@ -44,6 +44,19 @@ Team.getByTournamentID = function(tournamentID, callback) {
 	});
 };
 
+Team.prototype.update = function(callback) {
+	connection.query("UPDATE Team SET teamName=?, state=?, school=? WHERE tournamentID=?, teamNumber=?, division=?", 
+	[this.name, this.state, this.school, this.tournamentID, this.number, this.division], function(err, row) {
+		if(err) {
+			console.log('ERR', err);
+			callback(err);
+		} else {
+			console.log('Updated team ' + row.teamNumber + ' from tournament ' + row.tournamentID + ' in division ' + row.division);
+			callback();
+		}
+	});
+};
+
 Team.prototype.addToTournament = function(callback) {
 	connection.query("INSERT INTO Team(tournamentID, teamNumber, division, teamName, state, school) VALUES(?, ?, ?, ?, ?, ?)",
 		[this.tournamentID, this.number, this.division, this.name, this.state, this.school], function(err, row) {
