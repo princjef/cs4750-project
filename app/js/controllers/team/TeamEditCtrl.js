93f3ff730302ff72b647c57097c14b4cbb8ce957
Richard Knoll <nirro64@gmail.com>
2013-11-29 01:34:06 -0500

    Fixed the edit team modal window to not use the team to be edited directly for the models of the inputs

--- app/js/controllers/team/TeamEditCtrl.js
+++ app/js/controllers/team/TeamEditCtrl.js
@@ -1,12 +1,24 @@
 angular.module('scoreApp').controller('TeamEditCtrl', ['$scope', '$modalInstance', '$http', 'team', 'states', function($scope, $modalInstance, $http, team, states) {
+	$scope.form = {};
 	$scope.states = states.getStates();
 	$scope.editTeam = team.get(); 
+	
+	$scope.form.tournamentID = $scope.editTeam.tournamentID;
+	$scope.form.number = $scope.editTeam.number;
+	$scope.form.division = $scope.editTeam.division;
+	$scope.form.name = $scope.editTeam.name;
+	$scope.form.state = $scope.editTeam.state;
+	$scope.form.school = $scope.editTeam.school;
+	
 	$scope.updateTeam = function() {
 		$http({
 			method:'POST',
 			url:'/tournament/' + $scope.editTeam.tournamentID + '/updateteam',
-			data:$scope.editTeam
+			data:$scope.form
 		}).success(function(data) {
+			$scope.editTeam.name = $scope.form.name;
+			$scope.editTeam.state = $scope.form.state;
+			$scope.editTeam.school = $scope.form.school;
 			$modalInstance.dismiss('success');
 		}).error(function(err) {
 
