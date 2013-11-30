19b71dae62a3db14a29da365564b3ec7f935e945
Jeff Principe <princjef@gmail.com>
2013-11-29 20:44:56 -0500

    added user permissions helper

---
+++ helper/permissions.js
@@ -0,0 +1,36 @@
+var connection = require('../sql/connection');
+var error = require('../sql/error');
+
+exports.organization = function(req, res, orgID, callback) {
+	if(!req.user || !req.user.username) {
+		res.send(401);
+	} else {
+		connection.query("SELECT * FROM BelongsTo WHERE username=? AND orgID=? LIMIT 1",
+				[req.user.username, orgID], function(err, rows) {
+			if(err) {
+				res.send(500, error.message(err));
+			} else if(rows.length === 0) {
+				res.send(401);
+			} else {
+				callback();
+			}
+		});
+	}
+};
+
+exports.tournament = function(req, res, orgID, callback) {
+	if(!req.user || !req.user.username) {
+		res.send(401);
+	} else {
+		connection.query("SELECT * FROM BelongsTo NATURAL JOIN RunBy WHERE username=? tournamentID=?",
+				[req.user.username, tournamentID], function(err, rows) {
+			if(err) {
+				res.send(500, error.message(err));
+			} else if(rows.length === 0) {
+				res.send(401);
+			} else {
+				callback();
+			}
+		});
+	}
+};
