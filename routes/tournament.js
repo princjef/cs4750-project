80a82a378c513a8785e680524baaaedbfeabde4c
Jeff Principe <princjef@gmail.com>
2013-11-29 20:45:22 -0500

    added permissions verification to tournament routes

--- routes/tournament.js
+++ routes/tournament.js
@@ -1,17 +1,20 @@
 var Tournament = require('../model/Tournament');
 var RunBy = require('../model/RunBy');
+var permissions = require('../helper/permissions');
 
 exports.info = function(req, res) {
-	var tournament = new Tournament({
-		id: req.params.id
-	});
+	permissions.tournament(req, res, req.params.id, function() {
+		var tournament = new Tournament({
+			id: req.params.id
+		});
 
-	tournament.getByID(function(err) {
-		if(err) {
-			res.send(500, err);
-		} else {
-			res.json(tournament.toJson());
-		}
+		tournament.getByID(function(err) {
+			if(err) {
+				res.send(500, err);
+			} else {
+				res.json(tournament.toJson());
+			}
+		});
 	});
 };
 
@@ -26,61 +29,67 @@ exports.levels = function(req, res) {
 };
 
 exports.create = function(req, res) {
-	var tournament = new Tournament({
-		name: req.body.name,
-		type: req.body.type,
-		location: req.body.location,
-		date: req.body.date
-	});
+	permissions.organization(req, res, req.body.organizationID, function() {
+		var tournament = new Tournament({
+			name: req.body.name,
+			type: req.body.type,
+			location: req.body.location,
+			date: req.body.date
+		});
 
-	tournament.create(function(err) {
-		if(err) {
-			res.send(500, err);
-		} else {
-			var runBy = new RunBy({
-				orgID: req.body.organizationID,
-				tournamentID: tournament.id
-			});
+		tournament.create(function(err) {
+			if(err) {
+				res.send(500, err);
+			} else {
+				var runBy = new RunBy({
+					orgID: req.body.organizationID,
+					tournamentID: tournament.id
+				});
 
-			runBy.create(function(err) {
-				if(err) {
-					res.send(500, err);
-				} else {
-					res.json(tournament.toJson());
-				}
-			});
-		}
+				runBy.create(function(err) {
+					if(err) {
+						res.send(500, err);
+					} else {
+						res.json(tournament.toJson());
+					}
+				});
+			}
+		});
 	});
 };
 
 exports.update = function(req, res) {
-	var tournament = new Tournament({
-		name: req.body.name,
-		type: req.body.type,
-		location: req.body.location,
-		date: req.body.date,
-		id: req.body.id
-	});
+	permissions.tournament(req, res, req.body.id, function() {
+		var tournament = new Tournament({
+			name: req.body.name,
+			type: req.body.type,
+			location: req.body.location,
+			date: req.body.date,
+			id: req.body.id
+		});
 
-	tournament.update(function(err) {
-		if(err) {
-			res.send(500, err);
-		} else {
-			res.json(tournament.toJson());
-		}
+		tournament.update(function(err) {
+			if(err) {
+				res.send(500, err);
+			} else {
+				res.json(tournament.toJson());
+			}
+		});
 	});
 };
 
 exports.getByOrganizationID = function(req, res) {
-	Tournament.getByOrganizationID(req.params.organizationID, function(err, tournaments) {
-		if(err) {
-			res.send(500, err);
-		} else {
-			var result = [];
-			tournaments.forEach(function(tournament) {
-				result.push(tournament.toJson());
-			});
-			res.json(result);
-		}
+	permissions.organization(req, res, req.params.organizationID, function() {
+		Tournament.getByOrganizationID(req.params.organizationID, function(err, tournaments) {
+			if(err) {
+				res.send(500, err);
+			} else {
+				var result = [];
+				tournaments.forEach(function(tournament) {
+					result.push(tournament.toJson());
+				});
+				res.json(result);
+			}
+		});
 	});
 };
