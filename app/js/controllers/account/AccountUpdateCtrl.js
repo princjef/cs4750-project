39638d7b485e11f46439e555911769c41962dff6
Matt Pearson-Beck <mjp2ff@virginia.edu>
2013-12-01 07:02:24 -0500

    Minor change to helper/permissions.js in user function. Added two files that I forgot in last commit.

---
+++ app/js/controllers/account/AccountUpdateCtrl.js
@@ -0,0 +1,23 @@
+angular.module('scoreApp').controller('AccountUpdateCtrl',
+	['$scope', '$http', 'alert', 'user', function($scope, $http, alert, user) {
+
+	$scope.form = {};
+
+	$scope.updateAccount = function() {
+		$http({
+			method: 'POST',
+			url: '/account/update',
+			data: $scope.form
+		}).success(function(res) {
+			if (res.status) {
+				alert.success('Successfully updated account!');
+				user.current();	// Update current user.
+			}
+			else {
+				alert.danger('Account update not successful!');
+			}
+		}).error(function(err) {
+			alert.danger(err);
+		});
+	};
+}]);
--- routes/account.js
+++ routes/account.js
@@ -40,7 +40,7 @@ exports.create = function(req, res) {
 };
 
 exports.update = function(req, res) {
-	permissions.user(req, res, req.user.username, function() {
+	permissions.user(req, res, function() {
 		var account = new Account({
 			username: req.user.username,
 			email: req.body.newEmail,
