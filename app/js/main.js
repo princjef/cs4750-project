bd7027932e2182a35f57e3cccf30c186e31aa1a5
Richard Knoll <nirro64@gmail.com>
2013-12-02 19:21:27 -0500

    Removed modal windows from the main.js routeProvider paths

--- app/js/main.js
+++ app/js/main.js
@@ -7,33 +7,13 @@ angular.module('scoreApp', ['ui.bootstrap', 'ngCookies', 'ngRoute'])
 				templateUrl: '/partials/splash.html',
 				controller: 'SplashCtrl'
 			})
-			.when('/tournament/new', {
-					templateUrl: '/partials/tournament/new.html',
-					controller: 'TournamentCreateCtrl'
-				})
-			.when('/tournament/:tournamentID/newevent', {
-					templateUrl: '/partials/tournament/newevent.html',
-					controller: 'TournamentAddEventCtrl'
-			})
 			.when('/tournament/:tournamentID/dashboard', {
 					templateUrl: '/partials/tournament/dashboard.html',
 					controller: 'TournamentDashCtrl'
 				})
-			.when('/tournament/:tournamentID/newteam', {
-					templateUrl: '/partials/team/newteam.html',
-					controller: 'TeamAddCtrl'
-				})
-			.when('/organization/new', {
-					templateUrl: '/partials/organization/new.html',
-					controller: 'OrganizationCreateCtrl'
-				})
 			.when('/organization/:organizationID/dashboard', {
 					templateUrl: '/partials/organization/dashboard.html',
 					controller: 'OrganizationDashCtrl'
-				})
-			.when('/account/new', {
-					templateUrl: '/partials/account/new.html',
-					controller: 'AccountCreateCtrl'
 				})
 			.when('/login', {
 					templateUrl: '/partials/account/loginPage.html',
@@ -42,10 +22,6 @@ angular.module('scoreApp', ['ui.bootstrap', 'ngCookies', 'ngRoute'])
 			.when('/account/update', {
 					templateUrl: '/partials/account/update.html',
 					controller: 'AccountUpdateCtrl'
-				})
-			.when('/official/new', {
-					templateUrl: '/partials/official/new.html',
-					controller: 'OfficialCreateCtrl'
 				})
 			.when('/official/:officialID/info', {
 					templateUrl: '/partials/official/info.html',
