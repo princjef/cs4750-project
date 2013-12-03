607337bbe74ce826a0fcbea09f3c5a74e546887f
Jeff Principe <princjef@gmail.com>
2013-12-03 09:03:43 -0500

    added automatic reconnect

--- sql/connection.js
+++ sql/connection.js
@@ -1,3 +1,26 @@
 var mysql = require('mysql');
 var config = require('./../config.json');
-module.exports = mysql.createConnection(config);
+
+var connection;
+
+var disconnected = function() {
+	connection = mysql.createConnection(config);
+
+	connection.connect(function(err) {
+		if(err) {
+			console.log("He's dead, Jim", err);
+			setTimeout(disconnected, 2000);
+		}
+	});
+
+	connection.on('error', function(err) {
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			disconnected();
+		}
+	});
+};
+
+setInterval(disconnected, 3600000);
+disconnected();
+
+module.exports = connection;
